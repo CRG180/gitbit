123916ea7d483f196f2604e287645ed50d82ae70
charles.gallagher07 <charles.gallagher07@gmail.com>
2023-11-10 13:31:05 -0600

    first changes

---
+++ MyClass.py
@@ -0,0 +1,18 @@
+
+class Dog:
+    AGE_COEF = 7
+    KIND = "cainie"
+    
+    def __init__(self, name:str, age:int) -> None:
+        self.name:str = name
+        self.age:int = age
+    
+    def convert_age(self)->int:
+        return self.age * Dog.AGE_COEF
+    
+    def talk(self)->None:
+        print(f"{self.name} says woff!!")
+        
+if __name__ == "__main__":
+    myDog = Dog("Ralf", 5)
+    myDog.talk()
