f2110151e54ab5639436708372dd57be9a9ac7d2
charles.gallagher07 <charles.gallagher07@gmail.com>
2023-11-10 13:33:39 -0600

    "conflict"

--- MyClass.py
+++ MyClass.py
@@ -11,7 +11,7 @@ class Dog:
         return self.age * Dog.AGE_COEF
     
     def talk(self)->None:
-        print(f"{self.name} says woff!!")
+        print(f"{self.name} says woff and my age is {self.convert_age()}!!")
         
 if __name__ == "__main__":
     myDog = Dog("Ralf", 5)
